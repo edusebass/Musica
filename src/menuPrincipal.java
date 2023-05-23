6dfda50fc085a7d6039a9aaca45cd13cbdaa148f
edusebass <eduardoalmachi123@hotmail.com>
2023-05-23 09:50:40 -0500

    upadte

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 public class menuPrincipal {
     public static void main(String[] args) {
         Scanner entrada = new Scanner( System.in );
-            Genero genero1 = new Genero("rock",
-                    "120bpm",
-                    1950,
-                    "EU",
-                    "Chuck Berry",
-                    "Festival de California" );
-            Genero genero2  = new Genero("chicha",
-                    "100bpm",
-                    1970,
-                    "Ecuador",
-                    "Glorita Cedeño",
-                    "Chicha Fest");
-        Genero genero3 = new Genero("pasillo","123bpm",1990,"Ecuador","Julio Jaramillo","Pasillo Ecuatoriano");
+        Genero genero1 = new Genero(
+                "rock",
+                "120bpm",
+                1950,
+                "EU",
+                "Chuck Berry",
+                "Festival de California" );
+        Genero genero2  = new Genero(
+                "chicha",
+                "100bpm",
+                1970,
+                "Ecuador",
+                "Glorita Cedeño",
+                "Chicha Fest");
+        Genero genero3 = new Genero(
+                "pasillo",
+                "123bpm",
+                1990,
+                "Ecuador",
+                "Julio Jaramillo",
+                "Pasillo Ecuatoriano");
 
         //con el set podemos cambiar al valor predeterminado en la clase
 
@@ -24,7 +32,7 @@ public class menuPrincipal {
 
 
 
-
+        ///////////////////////
         System.out.println("Ingrese el año a cambiar: ");
         int aniotemporal = entrada.nextInt();
         genero1.setAnio(aniotemporal);
